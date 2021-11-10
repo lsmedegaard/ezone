ce8c01c0d0924d3f7421fd24e86039f840998f33
lasse <lass948m@stud.kea.dk>
2021-11-10 20:31:21 +0100

    days work

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let progress = "games";
+
 function loadJSON() {
   fetch("data.json")
     .then((response) => response.json())
@@ -12,25 +14,54 @@ function loadJSON() {
 
 function prepareObjects(json) {
   const template = document.querySelector("template");
-  let noSpaceStrings = [];
-  json.types.forEach((type) => {
-    let typeArray = type.split(" ");
-    typeArray = typeArray.join("");
-    typeArray = typeArray.toLowerCase();
-    console.log(typeArray);
+  let jsonProgress;
+  if (progress === "types") {
+    jsonProgress = json.types;
+  }
+  if (progress === "games") {
+    jsonProgress = json.games;
+  }
+  if (progress === "areas") {
+    jsonProgress = json.areas;
+  }
 
-    noSpaceStrings.push(typeArray);
-  });
+  function prepareJsonData(json) {
+    let noSpaceStrings = [];
+    let typeArray = [];
+    jsonProgress.forEach((type) => {
+      /* = type.split("") */
+      if (type.includes(":")) {
+        typeArray = type.split(":");
+        console.log("something contains a : ");
+        /* typeArray.pop(`${typeArray.indexOf(":")}`); */
+      }
+      if (type.includes(" ")) {
+        typeArray = type.split(" ");
+        console.log("something contains a space ");
+      }
+      typeArray = type.split(":");
+      typeArray = type.split(" ");
+      typeArray = typeArray.join("");
+      /* typeArray.pop(`${typeArray.indexOf(":")}`); */
+      noSpaceStrings.push(typeArray);
+      /*  typeArray = typeArray.toLowerCase() */
 
+      console.log(typeArray);
+    });
+
+    return noSpaceStrings;
+  }
+
+  let noSpaceStrings = prepareJsonData(json);
   let j = 0;
-  json.types.forEach((types) => {
+  jsonProgress.forEach((types) => {
     const gameChoiceType = template.content.cloneNode(true);
     gameChoiceType.querySelector("div").id = "wrapper_" + noSpaceStrings[j];
     gameChoiceType.querySelector("input").id = "input_" + noSpaceStrings[j];
     gameChoiceType.querySelector("input").name = "game_types";
     gameChoiceType.querySelector("input").value = noSpaceStrings[j];
     gameChoiceType.querySelector("label").setAttribute("for", noSpaceStrings[j]);
-    gameChoiceType.querySelector("label").textContent = json.types[j].toUpperCase();
+    gameChoiceType.querySelector("label").textContent = jsonProgress[j].toUpperCase();
 
     gameChoiceType.querySelector(`#wrapper_${noSpaceStrings[j]}`).style.backgroundImage = `url(images/types/${noSpaceStrings[j]}.jpg)`;
     document.querySelector(".checkboxes").appendChild(gameChoiceType);
