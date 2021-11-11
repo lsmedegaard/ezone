13a7dba386afb23fc30fd14b7a9f9c02ea0e504f
JonasRossen <jona7906@stud.kea.dk>
2021-11-11 15:52:11 +0100

    submitting to restdb

---
+++ restdb.js
@@ -0,0 +1,24 @@
+"use strict";
+/* 
+var request = require("request");
+
+var options = {
+  method: "POST",
+  url: "https://ezoneform-b1df.restdb.io/rest/userforms",
+  headers: { "cache-control": "no-cache", "x-apikey": "a7223fce0ea590f8d666e6060b064717283ee", "content-type": "application/json" },
+  body: { field1: "xyz", field2: "abc" },
+  json: true,
+};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+}); */
+
+/* export const endpoint = "https://ezoneform-b1df.restdb.io/rest/userforms";
+
+export const headers = {
+  "Content-Type": "application/json; charset=utf-8",
+  "x-apikey": "a7223fce0ea590f8d666e6060b064717283ee",
+  "cache-control": "no-cache", */
